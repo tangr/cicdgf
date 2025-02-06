95e7069848db3c6811313729dbd66b628076c465
tangshoubin <tangshoubin@itiger.com>
2025-02-06 21:27:34 +0800

    update

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
@@ -12,7 +13,23 @@ import (
 	"cicdgf/internal/controller/user"
 )
 
+type CasConfig struct {
+	ServerURL   string // CAS服务器地址 例如: https://cas.example.com/cas
+	Service     string // 本服务地址 例如: http://localhost:8199
+	ValidateURL string // 票据验证地址
+	LoginURL    string // 登录地址
+	LogoutURL   string // 登出地址
+}
+
 var (
+	cfg = CasConfig{
+		ServerURL:   "https://sso-prod.yax.tech/cas",
+		Service:     "http://localhost:8000",
+		ValidateURL: "/serviceValidate",
+		LoginURL:    "/login",
+		LogoutURL:   "/logout",
+	}
+
 	Main = gcmd.Command{
 		Name:  "main",
 		Usage: "main",
@@ -35,6 +52,15 @@ var (
 
 				group.GET("/test", func(r *ghttp.Request) {
 					r.Response.Write("Home Page")
+				})
+				group.GET("/login", func(r *ghttp.Request) {
+					// 重定向到CAS服务器
+					redirectURL := fmt.Sprintf("%s%s?service=%s/callback",
+						cfg.ServerURL,
+						cfg.LoginURL,
+						cfg.Service,
+					)
+					r.Response.RedirectTo(redirectURL)
 				})
 
 			})
