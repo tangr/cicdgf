47d8ded26b4370dcbb6c0955827c7edfe2065a4d
tangshoubin <tangshoubin@itiger.com>
2025-02-06 18:02:39 +0800

    update

--- internal/controller/ui/ui_v1_ui.go
+++ internal/controller/ui/ui_v1_ui.go
@@ -3,10 +3,20 @@ package ui
 import (
 	v1 "cicdgf/api/ui/v1"
 	"context"
+
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
 )
 
-func (c *ControllerV1) Ui(ctx context.Context, req *v1.UiReq) (res *v1.UiRes, err error) {
-	return &v1.UiRes{
-		Content: `<html>Your UI Content</html>`,
-	}, nil
+func (c *ControllerV1) Ui(ctx context.Context, req *v1.UiReq) (response *ghttp.Response, err error) {
+	r := g.RequestFromCtx(ctx)
+
+	err = r.Response.WriteTpl("cicd/list.html", g.Map{
+		"Title":          "欢迎页面",
+		"Content":        "这是通过模板渲染的内容",
+		"url":            "/",
+		"pipelines":      "pipelines",
+		"newPipelineUrl": "/pipelines/new",
+	})
+	return nil, err
 }
