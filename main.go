1c8eca0a5281eb619e2fc1547ea580284c246c9d
tangshoubin <tangshoubin@itiger.com>
2025-02-06 17:52:09 +0800

    update

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"cicdgf/internal/boot"
 	_ "cicdgf/internal/boot"
 	_ "cicdgf/internal/packed"
 
-	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
-
-	"github.com/gogf/gf/v2/os/gctx"
-
 	"cicdgf/internal/cmd"
+
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+	"github.com/gogf/gf/v2/os/gctx"
 )
 
 func main() {
-	boot.InitView()
 	cmd.Main.Run(gctx.GetInitCtx())
 }
