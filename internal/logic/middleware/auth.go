06d15397a6e797596b834764443ac0899d13e754
tangshoubin <tangshoubin@itiger.com>
2025-02-08 11:58:21 +0800

    update

--- internal/logic/middleware/auth.go
+++ internal/logic/middleware/auth.go
@@ -34,7 +34,7 @@ func AuthMiddleware(r *ghttp.Request) {
 
 	// 定期验证 SSO session
 	lastValidateTime := r.Session.MustGet("last_validate_time").Time()
-	if time.Since(lastValidateTime) > 5*time.Minute {
+	if time.Since(lastValidateTime) > 500*time.Minute {
 		callbackURL := fmt.Sprintf("%s/callback", common.Cfg.ServiceURL)
 		_, err := common.ValidateSSOSession(r.Context(), ticket, callbackURL)
 		if err != nil {
