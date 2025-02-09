3f5531f935a7c4d9a002b95200e873349b1ee3cb
tang <tangshoubin@163.com>
2025-02-09 22:34:32 +0800

    update

--- internal/controller/ui/ui_new.go
+++ internal/controller/ui/ui_new.go
@@ -66,6 +66,11 @@ type AgentUpdateReq struct {
 	g.Meta `path:"/agents/{id}/put" tags:"ui" method:"post" summary:"ui console index"`
 }
 
+// pipelines
+type PipelineGetListReq struct {
+	g.Meta `path:"/pipelines" tags:"ui" method:"get" summary:"ui console index"`
+}
+
 type IUiV1 interface {
 	Ui(ctx context.Context, req *UiReq) (res *ghttp.Response, err error)
 
@@ -86,6 +91,8 @@ type IUiV1 interface {
 	AgentCreate(ctx context.Context, req *AgentCreateReq) (res *ghttp.Response, err error)
 	AgentGetOne(ctx context.Context, req *AgentGetOneReq) (res *ghttp.Response, err error)
 	AgentUpdate(ctx context.Context, req *AgentUpdateReq) (res *ghttp.Response, err error)
+
+	PipelineGetList(ctx context.Context, req *PipelineGetListReq) (res *ghttp.Response, err error)
 }
 
 type ControllerV1 struct{}
