c75abc55dcd1a57dfac339b7dccc4d852ebfea47
tang <tangshoubin@163.com>
2025-02-09 16:09:00 +0800

    update

--- internal/service/agent.go
+++ internal/service/agent.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gtime"
 )
 
 var Agent = agentService{}
@@ -14,15 +15,20 @@ type agentService struct{}
 
 type ListAgents struct {
 	Id         int    `json:"id"`
-	Username   string `json:"username"`
-	Groups     string `json:"groups"`
+	Agent_name string `json:"agent_name"`
+	Ipaddr     string `json:"ipaddr"`
 	Updated_at int    `json:"updated_at"`
 }
 
-func (s *agentService) GetListAgents(ctx context.Context) (users []ListAgents, err error) {
+type AgentDetail struct {
+	Agent_name string `json:"agent_name"`
+	Ipaddr     string `json:"ipaddr"`
+}
+
+func (s *agentService) GetListAgents(ctx context.Context) (agents []ListAgents, err error) {
 	if err = dao.CicdAgent.Ctx(ctx).
-		Fields("id,agent_name,updated_at").
-		Scan(&users); err != nil {
+		Fields("id,agent_name,ipaddr,updated_at").
+		Scan(&agents); err != nil {
 		return nil, gerror.Wrap(err, "get agents failed")
 	}
 
@@ -33,8 +39,8 @@ func (s *agentService) New(agent_name string, agent_ipaddr string) int64 {
 	ctx := context.Background()
 
 	new_agent := g.Map{
-		"agent_name":   agent_name,
-		"agent_ipaddr": agent_ipaddr,
+		"agent_name": agent_name,
+		"ipaddr":     agent_ipaddr,
 	}
 
 	result, err := dao.CicdAgent.Ctx(ctx).Insert(new_agent)
@@ -45,6 +51,48 @@ func (s *agentService) New(agent_name string, agent_ipaddr string) int64 {
 	agent_id, err := result.LastInsertId()
 	if err != nil {
 		g.Log().Error(ctx, err)
+	}
+
+	return agent_id
+}
+
+func (s *agentService) GetAgent(agent_id string) (*AgentDetail, error) {
+	ctx := context.Background()
+
+	record, err := dao.CicdAgent.Ctx(ctx).
+		Fields("agent_name, ipaddr").
+		Where("id=?", agent_id).
+		One()
+
+	if err != nil {
+		g.Log().Error(ctx, err)
+		return nil, err
+	}
+
+	return &AgentDetail{
+		Agent_name: record["agent_name"].String(),
+		Ipaddr:     record["ipaddr"].String(),
+	}, nil
+}
+
+func (s *agentService) Update(agent_id string, agent_name string, agent_ipaddr string) string {
+	ctx := context.Background()
+
+	new_agent := g.Map{
+		"agent_name": agent_name,
+		"ipaddr":     agent_ipaddr,
+		"updated_at": gtime.Now().Timestamp(),
+	}
+	g.Log().Debug(ctx, "Update new_agent:", new_agent)
+	g.Log().Debug(ctx, "Update agent_ipaddr:", agent_ipaddr)
+
+	result, err := dao.CicdAgent.
+		Ctx(ctx).
+		Where("id=?", agent_id).
+		Update(new_agent)
+	if err != nil {
+		g.Log().Error(ctx, err)
+		g.Log().Error(ctx, result)
 	}
 
 	return agent_id
