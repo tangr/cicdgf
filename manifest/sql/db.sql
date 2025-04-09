6f77df2bbbd5892da450175507749c242c049699
tangshoubin <tangshoubin@itiger.com>
2025-04-09 10:40:04 +0800

    update

--- manifest/sql/db.sql
+++ manifest/sql/db.sql
@@ -14,80 +14,82 @@ CREATE TABLE `user` (
 CREATE TABLE IF NOT EXISTS `cicd_pipeline` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `pipeline_name` varchar(255) NOT NULL,
-  `group_id` int(11) NOT NULL,
-  `agent_id` int(11) NOT NULL,
-  `concurrency` int(11) DEFAULT 1,
+  `s_token` varchar(255) NOT NULL,
+  `group_id` int(11) UNSIGNED NOT NULL,
+  `agent_id` int(11) UNSIGNED NOT NULL,
+  `concurrency` int(11) UNSIGNED DEFAULT 1,
   `body` JSON NOT NULL,
   -- `realpipelineid` int(11) DEFAULT NULL,
   -- `version` int(11) DEFAULT NULL,
   `author` varchar(255) NOT NULL,
-  `updated_at` bigint(10) NOT NULL,
+  `updated_at` bigint(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `pipeline_name` (`pipeline_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `cicd_job` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `pipeline_id` int(11) NOT NULL,
-  `agent_id` int(11) NOT NULL,
-  `concurrency` int(11) DEFAULT 1,
-  `job_type` varchar(255) NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `pipeline_id` int(11) UNSIGNED NOT NULL,
+  `agent_id` int(11) UNSIGNED NOT NULL,
+  `concurrency` int(11) UNSIGNED DEFAULT 1,
+  `job_type` varchar(255) UNSIGNED NOT NULL,
   `job_status` varchar(255) NOT NULL,
   `script` JSON NOT NULL,
   `comment` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
-  `created_at` bigint(10) NOT NULL,
+  `created_at` bigint(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `cicd_log` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `pipeline_id` int(11) NOT NULL,
-  `agent_id` int(11) NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `pipeline_id` int(11) UNSIGNED NOT NULL,
+  `agent_id` int(11) UNSIGNED NOT NULL,
   `job_type` varchar(255) NOT NULL,
-  `job_id` int(11) NOT NULL,
+  `job_id` int(11) UNSIGNED NOT NULL,
   `task_status` varchar(255) NOT NULL,
   `ipaddr` varchar(255) NOT NULL,
-  `updated_at` bigint(10) NOT NULL,
+  `updated_at` bigint(10) UNSIGNED NOT NULL,
   `output` longtext DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `log_id` (`job_id`, `ipaddr`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `cicd_package` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `pipeline_id` int(11) NOT NULL,
-  `job_id` int(11) NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `pipeline_id` int(11) UNSIGNED NOT NULL,
+  `job_id` int(11) UNSIGNED NOT NULL,
   `job_status` varchar(255) NOT NULL,
   `package_name` varchar(255) NOT NULL,
-  `created_at` bigint(10) NOT NULL,
+  `created_at` bigint(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `job_id` (`job_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `cicd_script` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `script_name` varchar(255) NOT NULL,
   `script_body` longtext NOT NULL,
   `author` varchar(255) NOT NULL,
-  `updated_at` bigint(10) NOT NULL,
+  `updated_at` bigint(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `script_name` (`script_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `cicd_agent` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `agent_name` varchar(255) NOT NULL,
+  `s_token` varchar(255) NOT NULL,
   `ipaddr` varchar(255) NOT NULL,
-  `updated_at` bigint(10) NOT NULL,
+  `updated_at` bigint(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `agent_name` (`agent_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `cicd_group` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `group_name` varchar(255) NOT NULL,
-  `parent_id` int(11) NOT NULL,
+  `parent_id` int(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -95,7 +97,7 @@ CREATE TABLE `cicd_user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `group_id` JSON NOT NULL,
-  `updated_at` bigint(10) NOT NULL,
+  `updated_at` bigint(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
