86fbc42294de29874b5748051af144e848ec77cd
tangshoubin <tangshoubin@itiger.com>
2025-06-03 20:52:58 +0800

    update

--- internal/controller/log/log_v1_update.go
+++ internal/controller/log/log_v1_update.go
@@ -6,9 +6,14 @@ import (
 	v1 "cicdgf/api/log/v1"
 	"cicdgf/internal/dao"
 	"cicdgf/internal/model/do"
+
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/util/gconv"
 )
 
 func (c *ControllerV1) Update(ctx context.Context, req *v1.UpdateReq) (res *v1.UpdateRes, err error) {
+	g.Log().Debugf(ctx, "sendMap: %s", gconv.String(req))
+
 	_, err = dao.CicdLog.Ctx(ctx).Data(do.CicdLog{
 		PipelineId: req.PipelineId,
 		AgentId:    req.AgentId,
